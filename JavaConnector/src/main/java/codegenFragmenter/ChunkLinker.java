72e4d87f87359f168f139978c08ae8c021f7e4f0
casinobear <smith.jesse@hotmail.com>
2025-08-31 14:50:32 +1000

    "Add Spotless Maven plugin and configure check goal"

--- JavaConnector/src/main/java/codegenFragmenter/ChunkLinker.java
+++ JavaConnector/src/main/java/codegenFragmenter/ChunkLinker.java
@@ -1,7 +1,7 @@
-//This is for looking through the list of chunks, and linking dependant fragments together.
-//Searches through each chunk, and checks if it contains the name of any defined functions;
-//If found, chunk containing function definition is appended to list.
-//Returns a list, containing all grouped chunks - each group being another list.
+// This is for looking through the list of chunks, and linking dependant fragments together.
+// Searches through each chunk, and checks if it contains the name of any defined functions;
+// If found, chunk containing function definition is appended to list.
+// Returns a list, containing all grouped chunks - each group being another list.
 
 package codegenFragmenter;
 
@@ -11,29 +11,31 @@ import java.util.Map;
 
 public class ChunkLinker {
 
-    public static List<List<String>> link(Map<String,String> functionChunks){
-        List<List<String>> linkedChunks = new ArrayList<>();
+  public static List<List<String>> link(Map<String, String> functionChunks) {
+    List<List<String>> linkedChunks = new ArrayList<>();
 
-        int i = 0;
-        for(String chunk : functionChunks.values()){
-            List<String> singleChunkList = new ArrayList<>();
-            //We can add the header chunk here, or we can do it elsewhere depending how prompts will be constructed.
-            //TODO: Decide whether to keep following line:
-//            singleChunkList.add(functionChunks.get("Header"));
-            singleChunkList.add(chunk);
-            linkedChunks.add(singleChunkList);
-            if (!chunk.startsWith("Header:")) { //Header chunk needs no linking. Should be sent with all prompts.
-                for(Map.Entry function : functionChunks.entrySet()){
-                    if(chunk.contains((CharSequence) function.getKey())){
-                        if(!linkedChunks.get(i).contains(function.getValue())){
-                            linkedChunks.get(i).add((String) function.getValue());
-                        }
-                    }
-                }
+    int i = 0;
+    for (String chunk : functionChunks.values()) {
+      List<String> singleChunkList = new ArrayList<>();
+      // We can add the header chunk here, or we can do it elsewhere depending how prompts will be
+      // constructed.
+      // TODO: Decide whether to keep following line:
+      //            singleChunkList.add(functionChunks.get("Header"));
+      singleChunkList.add(chunk);
+      linkedChunks.add(singleChunkList);
+      if (!chunk.startsWith(
+          "Header:")) { // Header chunk needs no linking. Should be sent with all prompts.
+        for (Map.Entry function : functionChunks.entrySet()) {
+          if (chunk.contains((CharSequence) function.getKey())) {
+            if (!linkedChunks.get(i).contains(function.getValue())) {
+              linkedChunks.get(i).add((String) function.getValue());
             }
-            i++;
+          }
         }
-
-        return linkedChunks;
+      }
+      i++;
     }
+
+    return linkedChunks;
+  }
 }
