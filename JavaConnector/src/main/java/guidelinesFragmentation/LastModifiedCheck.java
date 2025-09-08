8a11079443fee18b84e7ddf657940d2793e8843f
Ben <108605249+ManawatuCoder@users.noreply.github.com>
2025-09-08 19:46:05 +1200

    minor edits

--- JavaConnector/src/main/java/guidelinesFragmentation/LastModifiedCheck.java
+++ JavaConnector/src/main/java/guidelinesFragmentation/LastModifiedCheck.java
@@ -1,4 +1,5 @@
-//Requests headers of provided url, and returns true and saves last-modified value if url 'last-modified'
+// Requests headers of provided url, and returns true and saves last-modified value if url
+// 'last-modified'
 // value differs from stored value.
 
 package guidelinesFragmentation;
@@ -11,29 +12,30 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class LastModifiedCheck {
-    public static boolean check(String url) throws Exception {
-        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-        conn.setRequestMethod("HEAD"); // we just want headers
-        conn.connect();
+  public static boolean check(String url) throws Exception {
+    HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+    conn.setRequestMethod("HEAD"); // we just want headers
+    conn.connect();
 
-        String lastModified = String.valueOf(conn.getLastModified());
+    String lastModified = String.valueOf(conn.getLastModified());
 
-        try {
-            Path lastModifiedPath = Path.of("src\\main\\java\\guidelinesFragmentation\\output\\last-modified.txt");
+    try {
+      Path lastModifiedPath =
+          Path.of("src\\main\\java\\guidelinesFragmentation\\output\\last-modified.txt");
 
-            if (!Files.exists(lastModifiedPath)) {
-                Files.writeString(lastModifiedPath, lastModified,
-                        StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-                return true;
-            }
+      if (!Files.exists(lastModifiedPath)) {
+        Files.writeString(
+            lastModifiedPath, lastModified, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        return true;
+      }
 
-            if (!Files.readString(lastModifiedPath).equals(lastModified)){
-                Files.writeString(lastModifiedPath, lastModified);
-                return true;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
+      if (!Files.readString(lastModifiedPath).equals(lastModified)) {
+        Files.writeString(lastModifiedPath, lastModified);
+        return true;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return false;
+  }
 }
