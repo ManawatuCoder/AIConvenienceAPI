884670bb0831a000f1ef15935235fb17fdb5da56
Lost-Packets <nate.pva@gmail.com>
2025-10-20 00:01:43 +1300

    Update POM

---
+++ Outputs/MergedOutputs/java_wrapper_2025-10-19_23-55.java
@@ -0,0 +1,11 @@
+package itest;
+public class Mini {
+  public String a(){ return "x"; }
+
+
+    /********************* GENERATED WRAPPER CODE *********************/
+    public void generated(){ System.out.println("ok"); }
+
+    /********************* END OF GENERATED CODE *********************/
+
+}
